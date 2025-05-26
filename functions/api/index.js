ad558247371cca2ab140f4028e39bd3396e7392e
Tomas-Mlynar <mlyt03@vse.cz>
2025-05-26 09:41:17 +0200

    Update index.js

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,1 +1,29 @@
+export async function onRequest(context) {
+  const BIN_ID = "683040d48a456b7966a44a27";
+  const API_KEY = "TVŮJ_X_MASTER_KEY";
+  const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
+
+  const { request } = context;
+  const method = request.method;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Master-Key": API_KEY
+  };
+
+  if (method === "GET") {
+    return fetch(BIN_URL, { headers });
+  }
+
+  if (method === "PUT") {
+    const body = await request.text();
+    return fetch(BIN_URL, {
+      method: "PUT",
+      headers,
+      body
+    });
+  }
+
+  return new Response("Method Not Allowed", { status: 405 });
+}
 
